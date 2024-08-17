b670889e4415839abd9826fcb15a48b1df29565e
Lakshay Manglani <lakshaymanglani2212@gmail.com>
2024-08-17 21:13:58 +0530

    chore(config): update configuration and environment files
    - Updated `.env.sample` with new environment variables.
    - Revised `env.config.js`, `kafka.config.js`, and `mail.config.js` to align with new configurations.

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -1,3 +1,17 @@
 import { config } from 'dotenv';
 
 config({ path: './.env' });
+
+const env = {
+  NODE_ENV: process.env.NODE_ENV || 'development',
+
+  KAFKA_CLIENT_ID: process.env.KAFKA_CLIENT_ID || 'email-service',
+  KAFKA_BROKERS_URI: process.env.KAFKA_BROKERS_URI.split(','),
+
+  SMTP_HOST: process.env.SMTP_HOST,
+  SMTP_PORT: parseInt(process.env.SMTP_PORT, 10),
+  SMTP_USERNAME: process.env.SMTP_USERNAME,
+  SMTP_PASSWORD: process.env.SMTP_PASSWORD,
+};
+
+export default env;
--- src/config/mail.config.js
+++ src/config/mail.config.js
@@ -1,13 +1,14 @@
 import { createTransport } from 'nodemailer';
+import env from './env.config.js';
+
 const smtp = {
-  host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
+  host: env.SMTP_HOST,
+  port: env.SMTP_PORT,
   auth: {
-    user: process.env.SMTP_USERNAME,
-    pass: process.env.SMTP_PASSWORD,
+    user: env.SMTP_USERNAME,
+    pass: env.SMTP_PASSWORD,
   },
 };
 
 const transporter = createTransport(smtp);
-
 export { transporter };
